d9150b344cef67b8b16c897bdf14f9bc174a500b
Oscar <oscar.montes201220@gmail.com>
2022-10-12 00:35:33 -0500

    Changes to use math.h

--- hello.c
+++ hello.c
@@ -2,4 +2,5 @@
 
 int main(void){
 	printf("Hello Wolrd!/n");
+	printf("The doble of 5 is %i", dobleInt(5));
 	return 0;
--- math.h
+++ math.h
@@ -1,3 +1,12 @@
 /* Encabezados de math.c */
 
 #define MAX_INT (2<sizeof(int))/2
+
+#include "math.h"
+
+int dobleInt(int a){
+	if(a> MAX_INT){
+		return -1:
+	}
+	return a*2
+}
