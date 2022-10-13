4c76111ddec325215e48cef4ef9f5575286ae61f
Oscar <oscar.montes201220@gmail.com>
2022-10-12 21:54:11 -0500

    new fun doubleDouble

--- math.c
+++ math.c
@@ -1,7 +1,17 @@
 /* Funciones Matematicas para nuestro codigo */
 
+#include "math.h"
+
 int dobleInt(int a) {
 	if(a > MAX_INT) {
 		return -1;
 	}
 	return a*2;
+}
+
+double dobleDouble(double a){
+	if(a > MAX_DOUBLE){
+		return -1;
+	}
+	return a*2;
+}
