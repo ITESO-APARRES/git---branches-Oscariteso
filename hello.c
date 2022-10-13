2f354ea8bba5fc6e5ab328b4da5231a06bfe24ff
Oscariteso <115527174+Oscariteso@users.noreply.github.com>
2022-10-12 23:17:47 -0500

    Delete hello.c

--- hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "math.h"
-
-int main(void){
-	printf("Hello Wolrd!/n");
-	printf("this is a second line/n");
-	printf("The doble of 5 is %i", dobleInt(5));
-	return 0;
-}
